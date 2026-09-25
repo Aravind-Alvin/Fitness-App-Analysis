b5f4c338ec4afe6eb39228e36008c2fcfb8aa1f0
Aravind M <aravindalvin007@gmail.com>
2026-09-25 20:00:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -102,7 +102,7 @@ if page == "🏠 Overview":
     st.markdown('<p class="main-title">STRAVA / Bellabeat Fitness Data Analytics</p>', unsafe_allow_html=True)
     st.markdown('<p class="sub-title">How consumers use their smart devices — insights for Bellabeat marketing strategy</p>',
                 unsafe_allow_html=True)
-    st.markdown("<style>[data-testid='stMetricLabel'] {color: #000000 !important; font-size: 18px !important; font-weight: 700 !important;}</style>", unsafe_allow_html=True)
+    st.markdown("<style>[data-testid='stMetricLabel'] {color: #000000 !important; font-size: 20px !important; font-weight: 700 !important;}</style>", unsafe_allow_html=True)
     st.markdown("<style>[data-testid='stMetricValue'] {color: #374c54 !important;}</style>", unsafe_allow_html=True)
 
     # -----------------------------------------------------------------------
